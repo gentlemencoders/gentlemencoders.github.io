7013e6e67afc59122866248aadbc25427d10314a
GentlemenCoders <support@gentlemencoders.com>
2020-10-17 12:01:55 -0700

    Port more resources

--- header.js
+++ header.js
@@ -1,7 +1,17 @@
+function toggleNav(element){
+    const parent = element.parentElement;
+    if (parent.className.includes("toggled")){
+        parent.className = parent.className.replace(/toggled/gm,'')
+    }
+    else{
+       parent.className += " toggled";
+    }
+}
+
 {
     const menus = {
-        "About":"/about",
-        "Videos":"",
+        "About":"about/",
+        "Videos":"https://www.youtube.com/channel/UC7l78rOzFxaHG-e5AT-MGWQ/videos",
         "Cameras":"",
         "Support":"",
         "Blog":"",
@@ -13,9 +23,9 @@
     let html = [
         `<header id="masthead" class="site-header stickyHeader" style="background-color:#333333"
         <a href="/" class="custom-logo-link" rel="home">
-        <img src="https://gentlemencoders.com/wp-content/uploads/2019/07/GC-Logo-White-Horizontal.png" class="custom-logo"</a>
+        <img src="${document.currentScript.getAttribute('root')}/images/GC-Logo-White-Horizontal.png" class="custom-logo"</a>
         <nav id="site-navigation" class="main-navigation" style="text-align:center">
-        <button class="menu-toggle pillbutton" aria-controls="primary-menu">Menu</button>
+        <button class="menu-toggle pillbutton" onclick="toggleNav(this)">Menu</button>
         <div class="menu-primary-navigation-container">
         <ul id="primary-menu">
         `];
