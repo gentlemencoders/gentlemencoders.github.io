ff2ca7fba0e4fd10e44a3fcb9abf95c172b5f777
Nik Bhatt <nik@gentlemencoders.com>
2024-01-21 10:19:10 -0800

    new website!

--- footer.js
+++ footer.js
@@ -8,32 +8,36 @@
             padding-right: 5px;
         }
     </style>
-    <footer id="colophon" class="site-footer" style="background-color:#333333;">
-            <div class="site-info" style="padding-left:10px;">
-                <div class="responsive whiteText" style="text-align:center;text-decoration:none">
-                    <h4>© ${new Date().getFullYear()} Gentlemen Coders, LLC</h4>
-                    <h4><a href="https://www.iubenda.com/terms-and-conditions/8228479">Terms and Conditions</a></h4>
-                    <div>
-                        <a href="http://facebook.com/gentlemencoders">
-                            <img class="icon" src="${root}/images/social/facebook_color.png"></img>
-                        </a>
-                        <a href="https://www.instagram.com/gentlemencoders/">
-                            <img class="icon" src="${root}/images/social/instagram_color.png"></img>
-                        </a>
-                        <a href="https://www.pinterest.com/gentscoders/">
-                            <img class="icon" src="${root}/images/social/pinterest_color.png"></img>
-                        </a>
-                        <a href="https://twitter.com/gentcoders">
-                            <img class="icon" src="${root}/images/social/twitter_color.png"></img>
-                        </a>
-                        <a href="https://www.youtube.com/channel/UC7l78rOzFxaHG-e5AT-MGWQ">
-                            <img class="icon" src="${root}/images/social/youtube_color.png"></img>
-                        </a>
-
-                    </div>
-                    <h4><a href="https://www.iubenda.com/privacy-policy/8228479">Privacy Policy</a></h4>
-                </div>
+    <footer id="colophon" class="site-footer" style="background-color:#222222;">
+    <div class="site-info" style="padding-left:10px;padding-top: 16px;">
+        <div class="responsive" style="text-align:center;text-decoration:none">
+            <div>
+                <a href="http://facebook.com/gentlemencoders">
+                    <img class="icon" src="${root}/images/social/facebook_color.png"></img>
+                </a>
+                <a href="https://www.instagram.com/gentlemencoders/">
+                    <img class="icon" src="${root}/images/social/instagram_color.png"></img>
+                </a>
+                <a href="https://twitter.com/gentcoders">
+                    <img class="icon" src="${root}/images/social/twitter_color.png"></img>
+                </a>
+                <a href="https://www.youtube.com/channel/UC7l78rOzFxaHG-e5AT-MGWQ">
+                    <img class="icon" src="${root}/images/social/youtube_color.png"></img>
+                </a>
             </div>
-        </footer>
+        </div>
+        <br>
+        <center><span class="footer-text">© ${new Date().getFullYear()} Gentlemen Coders, LLC</span></center>
+       <div class="footer-grid">
+            <span><a href="https://www.gentlemencoders.com/contact-us/index.html">Contact Us</a></span>
+            <span><a href="https://www.gentlemencoders.com/about/index.html">About</a></span>
+            <span><a href="https://www.gentlemencoders.com/support/index.html">Support</a></span>
+            <span><a href="https://www.gentlemencoders.com/extended-raw/index.html">Cameras</a></span>
+            <span><a href="https://www.iubenda.com/privacy-policy/8228479">Privacy Policy</a></span>
+            <span><a href="https://www.iubenda.com/terms-and-conditions/8228479">Terms and Conditions</a></span>
+        </div>
+        <h5>&nbsp;</h5>
+</div>
+</footer>
     `
 }
--- header.js
+++ header.js
@@ -11,21 +11,25 @@ function toggleNav(element){
 {
     const root = document.currentScript.src.substring(0,document.currentScript.src.lastIndexOf('/'));
     const menus = {
-        "Downloads":`${root}/downloads/`,
-        "Mailing List":"https://gentlemencoders.us15.list-manage.com/subscribe?u=03bf04c551d8bddf44492e889&id=3f1d84feef",
-        "Contact Us":`${root}/contact-us`,
-        "Blog":`${root}/blog`,
-        "Support":`${root}/support`,
-        "Cameras":`${root}/extended-raw/`,
-        "Videos":"https://www.youtube.com/channel/UC7l78rOzFxaHG-e5AT-MGWQ/videos",
-        "About":`${root}/about/`,
-    }
+        "Contact Us":`${root}/contact-us/index.html`,
+        "Cameras":`${root}/extended-raw/index.html`,
+        "Support":`${root}/support/index.html`,
+}
 
     let html = [
-        `<header id="masthead" class="site-header stickyHeader" style="background-color:#333333">
-        <a href="/"><img src="${root}/images/GC-Logo-White-Horizontal.png" class="custom-logo"></a>
+        `<header id="masthead" class="site-header stickyHeader" style="background-color:#222222">
+        <a href="/"><img src="${root}/images/GC_Logo_White_390x120.png" class="custom-logo"></a>
         <nav id="site-navigation" class="main-navigation" style="text-align:center">
-        <button class="menu-toggle pillbutton" onclick="toggleNav(this)">Menu</button>
+        <div class="menu-toggle navbutton" onclick="toggleNav(this)">	
+            <svg width="24px" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 384.97 384.97">
+                <path d="M12.03,84.212h360.909c6.641,0,12.03-5.39,12.03-12.03c0-6.641-5.39-12.03-12.03-12.03H12.03
+                    C5.39,60.152,0,65.541,0,72.182C0,78.823,5.39,84.212,12.03,84.212z"/>
+                <path d="M372.939,180.455H12.03c-6.641,0-12.03,5.39-12.03,12.03s5.39,12.03,12.03,12.03h360.909c6.641,0,12.03-5.39,12.03-12.03
+                    S379.58,180.455,372.939,180.455z"/>
+                <path d="M372.939,300.758H12.03c-6.641,0-12.03,5.39-12.03,12.03c0,6.641,5.39,12.03,12.03,12.03h360.909
+                    c6.641,0,12.03-5.39,12.03-12.03C384.97,306.147,379.58,300.758,372.939,300.758z"/>
+            </svg>
+</div>
         <div class="menu-primary-navigation-container">
         <ul id="primary-menu">
         `];
